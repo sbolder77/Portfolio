f96091c965ee24fd34b5cf8265f3bd880487059a
Simon Bolder <simonbolder1@gmail.com>
2023-10-29 17:27:15 +0000

    SSD Final

--- Code/SSD/Coding/SSDCoding/towersofhanoi.py
+++ Code/SSD/Coding/SSDCoding/towersofhanoi.py
@@ -1,4 +1,3 @@
-'''Module with function to calculate moves and print'''
 runcount = 0 
 
 def Hanoi(n, src, dst, tmp):
@@ -10,7 +9,5 @@ def Hanoi(n, src, dst, tmp):
         Hanoi(n - 1, tmp, dst, src)
 
 disks = int(input('Enter the number of disks: '))
-
 Hanoi(disks,0,2,1)
-
 print('Moved: ' + str(runcount) + ' times')
--- Code/SSD/Coding/SSDCoding/ukpostcode.py
+++ Code/SSD/Coding/SSDCoding/ukpostcode.py
@@ -5,7 +5,6 @@ POSTCODE_RE = re.compile('^[A-Z]{1,2}[0-9]{1,2}[A-Z]? [0-9][A-Z]{2}$')
 
 def normalise_postcode(postcode):
     """Return a normalised postcode if valid, or None if not."""
-
     postcode = NON_ALPHA_RE.sub('', postcode.upper())
     postcode = postcode[:-3] + ' ' + postcode[-3:]
     if POSTCODE_RE.match(postcode):
@@ -16,5 +15,4 @@ def normalise_postcode(postcode):
     return None
 
 postcode = str(input('Enter postcode: '))
-
 normalise_postcode(postcode)
